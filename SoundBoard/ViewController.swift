53e860dd0df8e21936a7301870e4f934fc834fec
Camavilca <orlando.camavilca@tecsup.edu.pe>
2019-10-02 12:13:26 -0500

    fix

--- SoundBoard/ViewController.swift
+++ SoundBoard/ViewController.swift
@@ -1,18 +1,9 @@
-//
-//  ViewController.swift
-//  SoundBoard
-//
-//  Created by Orlando Camavilca Chavez on 10/2/19.
-//  Copyright © 2019 Orlando Camavilca Chavez. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
